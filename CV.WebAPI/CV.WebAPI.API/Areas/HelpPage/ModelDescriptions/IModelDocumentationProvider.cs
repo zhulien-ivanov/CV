6cdb77346eada99517e0ff21a92e03bb9429652d
zh <izhulien@yahoo.com>
2016-01-07 16:03:56 +0200

    Add Initial Template for WebAPI

---
+++ CV.WebAPI/CV.WebAPI.API/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace CV.WebAPI.API.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ CV.WebAPI/CV.WebAPI.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CV.WebAPI.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
