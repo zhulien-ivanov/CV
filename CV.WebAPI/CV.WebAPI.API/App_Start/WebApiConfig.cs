e39307893f796abe6d14e825edec60fb068f3ff6
zh <izhulien@yahoo.com>
2016-01-07 21:27:42 +0200

    Enable CORS

--- CV.WebAPI/CV.WebAPI.API/App_Start/WebApiConfig.cs
+++ CV.WebAPI/CV.WebAPI.API/App_Start/WebApiConfig.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace CV.WebAPI.API
+﻿namespace CV.WebAPI.API
 {
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            var corsAttr = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsAttr);
 
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
