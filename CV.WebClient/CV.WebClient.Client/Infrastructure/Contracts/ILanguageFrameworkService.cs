cabffc1bec59e99e439b195c86bc57a018c88b09
zh <izhulien@yahoo.com>
2016-01-09 22:28:47 +0200

    Add Service contracts and implementations for consuming the WebAPI

---
+++ CV.WebClient/CV.WebClient.Client/Infrastructure/Contracts/ILanguageFrameworkService.cs
@@ -0,0 +1,15 @@
+﻿namespace CV.WebClient.Client.Infrastructure.Contracts
+{
+    using System.Collections.Generic;
+
+    using CV.Common.ViewModels;
+
+    public interface ILanguageFrameworkService
+    {
+        IEnumerable<LanguageFrameworkIconViewModel> GetAllFrameworksPartial();
+
+        LanguageFrameworkDetailedViewModel GetFrameworkDetails(int id);
+
+        IEnumerable<LanguageFrameworkDetailedViewModel> GetFrameworksByLanguage(int id);
+    }
+}
