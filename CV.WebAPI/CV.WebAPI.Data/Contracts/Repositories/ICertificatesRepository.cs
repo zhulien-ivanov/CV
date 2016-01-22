61ed8b20ab8d3c9bf5519f6963614bfca0cc9847
zh <zhulienivanov@yahoo.com>
2016-01-22 15:00:03 +0200

    Add repository contract and implementation for the certificates

---
+++ CV.WebAPI/CV.WebAPI.Data/Contracts/Repositories/ICertificatesRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CV.WebAPI.Data.Contracts.Repositories
+{
+    using System.Collections.Generic;
+
+    using CV.Common.ViewModels;
+
+    using CV.WebAPI.Models;
+
+    public interface ICertificatesRepository
+    {
+        IEnumerable<CertificateViewModel> GetAll();
+    }
+}
